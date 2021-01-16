08dd88b35c0244560eefffd9c5bd677a1c9b3984
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 13:00:58 +0100

    Exercice 2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("Bonjour, je suis le message n° " + num);
+                num++;
+            }
+        }
+    }
+}
